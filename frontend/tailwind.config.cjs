64129c7a6556dcb79a4c2ce134e68ca7234bf0c9
julien <gorguetmoura@gmail.com>
2025-03-31 10:40:39 -0300

    Ajustando configuração do Tailwind

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/views/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/App.vue",
+    "./src/main.ts"
   ],
   theme: {
     extend: {
