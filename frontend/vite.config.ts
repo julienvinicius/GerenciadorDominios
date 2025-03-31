0d43df2dcd1275f8e4065211c03f60bcc7905348
julien <gorguetmoura@gmail.com>
2025-03-31 10:08:07 -0300

    ajustes fronte vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,11 +13,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
       },
       '/sanctum': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
         secure: false
       }
